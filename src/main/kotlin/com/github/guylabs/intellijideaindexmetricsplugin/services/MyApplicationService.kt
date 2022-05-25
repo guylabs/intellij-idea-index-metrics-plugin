30234d1f0a95cc6c0f17364510cfe6d842900c35
GitHub Action <action@github.com>
2022-05-25 07:53:57 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/guylabs/intellijideaindexmetricsplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.guylabs.intellijideaindexmetricsplugin.services
+
+import com.github.guylabs.intellijideaindexmetricsplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/guylabs/intellijideaindexmetricsplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/guylabs/intellijideaindexmetricsplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.guylabs.intellijideaindexmetricsplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.guylabs.intellijideaindexmetricsplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
